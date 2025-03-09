18013554ddef06fa51e9dae3005703218e64a291
Ankush51620 <164299015+Ankush51620@users.noreply.github.com>
2025-03-09 10:49:12 +0530

    edit

--- src/app.js
+++ src/app.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//fjfgjfgh
+//zcxzxczxczxc
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.get('/about', (req, res) => {
+    res.send('About Us');
 });
 
 app.listen(port, () => {
